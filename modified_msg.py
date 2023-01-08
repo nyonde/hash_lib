bc8cd8024060fbe12670187048faef3ab83dfba3
shemkengere <68227753+nyonde@users.noreply.github.com>
2023-01-08 14:29:14 +0300

    Update and rename orginal_msg.py to modified_msg.py

--- modified_msg.py
+++ modified_msg.py
@@ -5,16 +5,16 @@ import hashlib
 # The original message
 original_message = "Hello, world!"
 
-# Calculate the hash of the original message
+# Calculating the hash of the original message
 original_hash = hashlib.sha256(original_message.encode())
 
 # The modified message
 modified_message = "Hello, world! How are you?"
 
-# Calculate the hash of the modified message
+# Calculating the hash of the modified message
 modified_hash = hashlib.sha256(modified_message.encode())
 
-# Compare the original hash and the modified hash
+# Comparing the original hash and the modified hash
 if original_hash == modified_hash:
     print("The message has not been modified.")
 else:
